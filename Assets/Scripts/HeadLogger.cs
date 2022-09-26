c4a9c342e60db6f957c663859096400a5b5889bd
TarVK <tarvankrieken@gmail.com>
2022-09-26 17:03:10 +0200

    Added some data logging

---
+++ Assets/Scripts/HeadLogger.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+public class HeadLogger : MonoBehaviour
+{
+    public GameObject focus;
+    string fileName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fileName = System.DateTime.Now.ToString("yyyy-MM-dd HH_mm_ss");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform headTransform = gameObject.transform;
+
+        // gameObject.
+        RaycastHit hit;
+        if (Physics.Raycast(headTransform.position, headTransform.TransformDirection(Vector3.forward), out hit, 20))
+        {
+            if (focus != null)
+                focus.transform.position = hit.point;
+
+            AddToLog(fileName, hit.point.ToString() + "@" + System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.ffff"));
+        }
+
+
+
+    }
+
+    void AddToLog(string fileName, string text)
+    {
+        string path = Application.dataPath + @"\data\"+fileName+".txt";
+        System.Text.UnicodeEncoding encode = new System.Text.UnicodeEncoding();
+        byte[] byteData = encode.GetBytes(text);
+        Debug.Log(text);
+        if (!File.Exists(path))
+        {
+            FileStream oFileStream = new FileStream(path, FileMode.Create);
+            oFileStream.Close();
+        }
+
+        using (StreamWriter stream = File.AppendText(path))
+        {
+            stream.WriteLine(text);
+        }
+    }
+}
