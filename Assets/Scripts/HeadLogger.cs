cb2773257642384fa096794e458b8b4027f3fb95
TarVK <tarvankrieken@gmail.com>
2022-09-28 13:13:47 +0200

    Added tracking enable/disable buttons + sampling speed setting

--- Assets/Scripts/HeadLogger.cs
+++ Assets/Scripts/HeadLogger.cs
@@ -10,12 +10,15 @@ using System.IO;
 public class HeadLogger : MonoBehaviour
 {
     public GameObject focus;
-    string fileName;
+    public double timesPerSec = 5;
+
+    private Boolean active = false;
+    private DateTime last = System.DateTime.Now;
+    private string fileName = "";
 
     // Start is called before the first frame update
     void Start()
     {
-        fileName = System.DateTime.Now.ToString("yyyy-MM-dd HH_mm_ss");
     }
 
     // Update is called once per frame
@@ -24,16 +27,37 @@ public class HeadLogger : MonoBehaviour
         Transform headTransform = gameObject.transform;
 
         RaycastHit hit;
-        if (Physics.Raycast(headTransform.position, headTransform.TransformDirection(Vector3.forward), out hit, 20))
+        if (active && Physics.Raycast(headTransform.position, headTransform.TransformDirection(Vector3.forward), out hit, 20))
         {
             if (focus != null)
+            {
+                focus.SetActive(true);
                 focus.transform.position = hit.point;
+            }
 
-            AddToLog(fileName, hit.point.ToString() + "@" + System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.ffff"));
+            DateTime now = System.DateTime.Now;
+            if (now.Subtract(last).Milliseconds > 1000 / timesPerSec)
+            {
+                AddToLog(fileName, hit.point.ToString() + "@" + now.ToString("yyyy/MM/dd HH:mm:ss.ffff"));
+                last = now;
+            }
+        }
+        else
+        {
+            if (focus != null) focus.SetActive(false);
         }
 
+        // Check for buttons
+        if (Input.GetButtonDown("XRI_Right_PrimaryButton"))
+            setActive(!active);
+    }
 
-
+    void setActive(Boolean active)
+    {
+        this.active = active;
+        if (active) {
+            fileName = System.DateTime.Now.ToString("yyyy-MM-dd HH_mm_ss");
+        }
     }
 
     void AddToLog(string fileName, string text)
