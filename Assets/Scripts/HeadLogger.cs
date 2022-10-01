de0198dbab828bb9708555b3b954c9a9e53accbd
TarVK <tarvankrieken@gmail.com>
2022-10-01 18:08:07 +0200

    Setup experiment flow (inputs)

--- Assets/Scripts/HeadLogger.cs
+++ Assets/Scripts/HeadLogger.cs
@@ -9,9 +9,20 @@ using System.IO;
 
 public class HeadLogger : MonoBehaviour
 {
-    public GameObject focus;
-    public double timesPerSec = 5;
+    private GameObject focus;
+    private GameObject focusLeftTop;
+    private GameObject focusRightTop;
+    private GameObject focusLeftBottom;
+    private GameObject focusRightBottom;
 
+    public float fovAngleYaw = 0.5f;
+    public float fovAnglePitch = 0.2f;
+
+    public double timesPerSec = 5;
+    public Boolean showTracker = false;
+    public Material trackerMaterial;
+
+    public int randomSeedData = 0;
     private Boolean active = false;
     private DateTime last = System.DateTime.Now;
     private string fileName = "";
@@ -19,6 +30,22 @@ public class HeadLogger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        focus = generateSphere();
+        focusLeftTop = generateSphere();
+        focusRightTop = generateSphere();
+        focusLeftBottom = generateSphere();
+        focusRightBottom = generateSphere();
+    }
+
+    GameObject generateSphere()
+    {
+        GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        float scale = 0.05f;
+        obj.transform.localScale = new Vector3(scale, scale, scale);
+        obj.SetActive(false);
+        obj.GetComponent<Collider>().enabled = false;
+        obj.GetComponent<Renderer>().material = trackerMaterial;
+        return obj;
     }
 
     // Update is called once per frame
@@ -26,37 +53,83 @@ public class HeadLogger : MonoBehaviour
     {
         Transform headTransform = gameObject.transform;
 
-        RaycastHit hit;
-        if (active && Physics.Raycast(headTransform.position, headTransform.TransformDirection(Vector3.forward), out hit, 20))
+        if(active)
         {
-            if (focus != null)
-            {
-                focus.SetActive(true);
-                focus.transform.position = hit.point;
-            }
+            RaycastHit hit;
+            Vector3 direction = headTransform.TransformDirection(Vector3.forward);
+            Vector3 right = headTransform.TransformDirection(Vector3.right);
+            Vector3 up = headTransform.TransformDirection(Vector3.up);
 
+            if (Physics.Raycast(headTransform.position, direction, out hit, 200))
+            {
+                focus.transform.position = hit.point;
+                focus.SetActive(showTracker);
+            }
+            else focus.SetActive(false);
+
+            if (Physics.Raycast(headTransform.position, rotate(rotate(direction, right, -fovAnglePitch), up, -fovAngleYaw), out hit, 200))
+            {
+                focusLeftTop.transform.position = hit.point;
+                focusLeftTop.SetActive(showTracker);
+            }
+            else focusLeftTop.SetActive(false);
+
+            if (Physics.Raycast(headTransform.position, rotate(rotate(direction, right, -fovAnglePitch), up, fovAngleYaw), out hit, 200))
+            {
+                focusRightTop.transform.position = hit.point;
+                focusRightTop.SetActive(showTracker);
+            }
+            else focusRightTop.SetActive(false);
+
+            if (Physics.Raycast(headTransform.position, rotate(rotate(direction, right, fovAnglePitch), up, -fovAngleYaw), out hit, 200))
+            {
+                focusLeftBottom.transform.position = hit.point;
+                focusLeftBottom.SetActive(showTracker);
+            }
+            else focusLeftBottom.SetActive(false);
+
+            if (Physics.Raycast(headTransform.position, rotate(rotate(direction, right, fovAnglePitch), up, fovAngleYaw), out hit, 200))
+            {
+                focusRightBottom.transform.position = hit.point;
+                focusRightBottom.SetActive(showTracker);
+            }
+            else focusRightBottom.SetActive(false);
+
+
+
+            // Add to log
             DateTime now = System.DateTime.Now;
             if (now.Subtract(last).Milliseconds > 1000 / timesPerSec)
             {
-                AddToLog(fileName, hit.point.ToString() + "@" + now.ToString("yyyy/MM/dd HH:mm:ss.ffff"));
+                AddToLog(fileName, 
+                    focus.transform.position.ToString() +
+                    focusLeftTop.transform.position.ToString() +
+                    focusRightTop.transform.position.ToString() +
+                    focusLeftBottom.transform.position.ToString() +
+                    focusRightBottom.transform.position.ToString() +
+                    "@" + now.ToString("yyyy/MM/dd HH:mm:ss.ffff"));
                 last = now;
             }
-        }
-        else
+        } else
         {
-            if (focus != null) focus.SetActive(false);
+            focus.SetActive(false);
+            focusLeftBottom.SetActive(false);
+            focusRightBottom.SetActive(false);
+            focusLeftTop.SetActive(false);
+            focusRightTop.SetActive(false);
         }
-
-        // Check for buttons
-        if (Input.GetButtonDown("XRI_Right_PrimaryButton"))
-            setActive(!active);
     }
 
-    void setActive(Boolean active)
+    Vector3 rotate(Vector3 direction, Vector3 axis, float angle)
+    {
+        return Quaternion.AngleAxis(angle, axis) * direction;
+    }
+
+    public void setActive(Boolean active)
     {
         this.active = active;
         if (active) {
-            fileName = System.DateTime.Now.ToString("yyyy-MM-dd HH_mm_ss");
+            fileName = System.DateTime.Now.ToString("yyyy-MM-dd HH_mm_ss")+" "+randomSeedData;
         }
     }
 
---
+++ Assets/Scripts/InitScene.cs
@@ -0,0 +1,201 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+
+public class InitScene : MonoBehaviour
+{
+    public int NrTilesHeight;
+    public int NrTilesWidth;
+    public float TileSize;
+
+    public GameObject pointer;
+    public GameObject pointingHighlighter;
+    public Vector3 pointerDir = new Vector3(0, 1f, -1);
+    public GameObject headset;
+
+    public bool randomTarget;
+    public bool downwardsWalls;
+
+    private HeadLogger logger;
+    private GameObject[,,] tiles;
+    private Object[] textures;
+
+    private bool searching = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logger = headset.GetComponent<HeadLogger>();
+        logger.randomSeedData = Random.Range(0, (int)1e8);
+        Random.InitState(logger.randomSeedData);
+
+        textures = Resources.LoadAll(@"TestImages", typeof(Texture2D));
+        if (textures.Length == 0) { 
+            Debug.LogWarning("No tile textures found. Abort scene initialization"); 
+            return; 
+        }
+        if (textures.Length == 1) { 
+            Debug.LogWarning("At least two images needes, one image found. Abort scene initialization"); 
+            return; 
+        }
+
+        tiles = new GameObject[4, NrTilesHeight, NrTilesWidth];
+        for (int wall_id = 0; wall_id < 4; wall_id++) {
+            for (int row = 0; row < NrTilesHeight; row++) {
+                for (int col = 0; col < NrTilesWidth; col++) {
+
+                    // Place tile at script object
+                    GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                    tiles[wall_id, row, col] = tile;
+                    tile.transform.parent = gameObject.transform;
+                    tile.transform.localPosition = new Vector3(0,0,0);
+
+                    // Move tile to correct position on wall
+                    if (downwardsWalls) {
+                        tile.transform.Translate((col + 0.5f - NrTilesWidth / 2) * TileSize, 
+                                                (row + 0.5f - NrTilesHeight / 2) * TileSize, 
+                                                (NrTilesWidth / 2) * TileSize);
+                    }
+                    else {
+                        tile.transform.Translate((col + 0.5f - NrTilesWidth / 2) * TileSize, 
+                                                (row + 0.5f) * TileSize, 
+                                                (NrTilesWidth / 2) * TileSize);
+                    }
+                                        
+                    // Scale tile
+                    tile.transform.localScale = new Vector3(TileSize * 0.1f, TileSize * 0.1f, TileSize * 0.1f);
+
+                    // Orient tile 
+                    tile.transform.localRotation = Quaternion.Euler(90, 0, 180);
+
+                    // Rotate around script position for each wall
+                    tile.transform.RotateAround(gameObject.transform.position, Vector3.up, 90 * wall_id);
+                }
+            }
+        }
+    }
+
+    void initializeSymbols()
+    {
+
+        // select target image
+        int target_id = 0;
+        if (randomTarget) target_id = Random.Range(0, textures.Length);
+
+        Texture2D target_texture = textures[target_id] as Texture2D;
+        Debug.Log(target_texture.name);
+        Material target_material = new Material(Shader.Find("Diffuse"));
+        target_material.mainTexture = target_texture;
+
+        List<Material> other_materials = new List<Material>();
+        for (int i = 0; i < textures.Length; i++)
+        {
+            if (i != target_id)
+            {
+                Texture texture = textures[i] as Texture2D;
+                Material material = new Material(Shader.Find("Diffuse"));
+                material.mainTexture = texture;
+                other_materials.Add(material);
+            }
+        }
+
+        //int target_wall = 0;
+        int target_wall = Random.Range(0, 4);
+        int target_row = Random.Range(0, NrTilesHeight);
+        int target_col = Random.Range(0, NrTilesWidth);
+
+        for (int wall_id = 0; wall_id < 4; wall_id++)
+        {
+            for (int row = 0; row < NrTilesHeight; row++)
+            {
+                for (int col = 0; col < NrTilesWidth; col++)
+                {
+                    GameObject tile = tiles[wall_id, row, col];
+
+                    // Apply random texture
+                    Material material;
+                    if (target_wall == wall_id && target_row == row && target_col == col)
+                    {
+                        material = target_material;
+                        tile.tag = "target";
+                    }
+                    else
+                    {
+                        material = other_materials[Random.Range(0, other_materials.Count)];
+                        tile.tag = "not target";
+                    }
+
+                    tile.GetComponent<Renderer>().material = material;
+                }
+            }
+        }
+    }
+
+    void hideSymbols()
+    {
+        Material material = new Material(Shader.Find("Diffuse"));
+        for (int wall_id = 0; wall_id < 4; wall_id++)
+        {
+            for (int row = 0; row < NrTilesHeight; row++)
+            {
+                for (int col = 0; col < NrTilesWidth; col++)
+                {
+                    GameObject tile = tiles[wall_id, row, col];
+                    tile.GetComponent<Renderer>().material = material;
+                }
+            }
+        }
+    }
+
+    public void setSearching(bool searching)
+    {
+        if (this.searching == searching) return;
+
+        this.searching = searching;
+        logger.setActive(searching);
+        if (searching)
+        {
+            initializeSymbols();
+        } else
+        {
+            hideSymbols();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Manage pointing
+        RaycastHit hit;
+        Transform pointerTransform = pointer.transform;
+        if (Physics.Raycast(pointerTransform.position, pointerTransform.TransformDirection(pointerDir.normalized), out hit, 200))
+        {
+            if (Input.GetButtonDown("XRI_Right_TriggerButton"))
+            {
+                if (hit.transform.gameObject.tag == "target")
+                {
+                    Debug.Log("Target found!");
+                    setSearching(false);
+                }
+                else
+                {
+                    Debug.Log("Invalid target");
+                }
+            }
+
+
+            pointingHighlighter.transform.position = hit.point;
+            pointingHighlighter.SetActive(true);
+        } else
+        {
+            pointingHighlighter.SetActive(false);
+        }
+
+
+
+        // Check for buttons
+        if (Input.GetButtonDown("XRI_Right_PrimaryButton"))
+            setSearching(true);
+    }
+}
